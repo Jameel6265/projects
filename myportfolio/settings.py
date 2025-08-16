7c169120d3a47bc30a857c48a2bb6348376eeb7a
Jameel6265 <101185692+Jameel6265@users.noreply.github.com>
2025-08-16 15:07:53 +0530

    Update settings.py

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for myportfolio project.
 
@@ -11,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -73,15 +75,20 @@ WSGI_APPLICATION = 'myportfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'advanced_portfolio',
+#         'USER':'root',
+#         'PASSWORD':'root',
+#         'HOST':'localhost',
+#         'PORT':'3306'
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'advanced_portfolio',
-        'USER':'root',
-        'PASSWORD':'root',
-        'HOST':'localhost',
-        'PORT':'3306'
-    }
+    'default': dj_database_url.config(
+        default=os.getenv("DATABASE_URL")  # Render will use this
+    )
 }
 
 
